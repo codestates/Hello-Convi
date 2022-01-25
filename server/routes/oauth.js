69afb5b9af09500ef18307fa074c234a8abe707b
shjthd92 <shjthd92@naver.com>
2022-01-26 07:56:51 +0900

    oauth added and etc

--- server/routes/oauth.js
+++ server/routes/oauth.js
@@ -2,7 +2,6 @@ const express = require('express');
 const router = express.Router();
 const Controller = require('../controllers/oauth');
 
-
 router.post('/', Controller.post);
 router.get('/', Controller.get);
 // router.delete('/:id', Controller.delete);
