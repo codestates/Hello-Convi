74fbc54b0c4bea103fc4ebb553cf3beaed2d9767
hkr0416 <hkr0416@gmail.com>
2022-01-26 11:16:40 +0900

    pull merge

--- client/src/pages/Callback.js
+++ client/src/pages/Callback.js
@@ -6,44 +6,43 @@ import { useNavigate } from 'react-router-dom';
 
 // let count = 0
 function Callback () {
-    const dispatch = useDispatch();
-    const navigate = useNavigate();
-    const {login} = actionCreators;
-    useEffect(async () => {
-        const url = new URL(window.location.href);
-        const authorizationCode = url.searchParams.get('code');
-        console.log(authorizationCode)
-        if (authorizationCode){
-            const userdata = await axios.post('http://localhost:8080/oauth', {authorizationCode}, {withCredentials:true})
-            console.log(userdata.data.data)
-            dispatch(login({ userId: userdata.data.data.id, email: userdata.data.data.email, nickname: userdata.data.data.nickname }));
-            navigate('/')
-        }        
-    },[])    
-    
-    // if(authorizationCode && count === 0){
-    //     count++
-        
-    //     axios.get('http://localhost:8080/oauth')
-    // }
-    
-    // useEffect(() => {
-    //     const url = new URL(window.location.href);
-    //     const authorizationCode = url.searchParams.get('code');
-    //     console.log(authorizationCode);
-    
-    //     if (authorizationCode/*  && count === 0 */) {
-    //       //count++;
-    //       axios.post('http://localhost:8080/oauth', { authorizationCode: authorizationCode }, { withCredentials: true })
-    //         .then(el => {
-    //           dispatch(login({ userId: el.data.data.id, email: el.data.data.email, nickname: el.data.data.nickname }));
-    //           navigate('/');
-    //         });
-    //     }
-    //   }, []) 
+  const dispatch = useDispatch();
+  const navigate = useNavigate();
+  const { login } = actionCreators;
+  useEffect(async () => {
+    const url = new URL(window.location.href);
+    const authorizationCode = url.searchParams.get('code');
+    console.log(authorizationCode);
+    if (authorizationCode) {
+      const userdata = await axios.post('http://localhost:8080/oauth', { authorizationCode }, { withCredentials: true });
+      console.log(userdata.data.data);
+      dispatch(login({ userId: userdata.data.data.id, email: userdata.data.data.email, nickname: userdata.data.data.nickname }));
+      navigate('/');
+    }
+  }, []);
 
+  // if(authorizationCode && count === 0){
+  //     count++
 
-return <div>...로딩중입니다</div>
+  //     axios.get('http://localhost:8080/oauth')
+  // }
+
+  // useEffect(() => {
+  //     const url = new URL(window.location.href);
+  //     const authorizationCode = url.searchParams.get('code');
+  //     console.log(authorizationCode);
+
+  //     if (authorizationCode/*  && count === 0 */) {
+  //       //count++;
+  //       axios.post('http://localhost:8080/oauth', { authorizationCode: authorizationCode }, { withCredentials: true })
+  //         .then(el => {
+  //           dispatch(login({ userId: el.data.data.id, email: el.data.data.email, nickname: el.data.data.nickname }));
+  //           navigate('/');
+  //         });
+  //     }
+  //   }, [])
+
+  return <div>...로딩중입니다</div>;
 }
 
 export default Callback;
